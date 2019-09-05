43116a14da77c7e3b5891537e731017612d2cdad
Thomas Lisankie <thomas@tomlisankie.com>
2019-09-05 14:24:30 -0400

    Saves keypair to a file-path

--- src/ssb_clj/core.clj
+++ src/ssb_clj/core.clj
@@ -3,5 +3,11 @@
             [byte-streams :as bs]))
 
 (def keypair (sign/keypair!))
-(def public-key-string (bs/to-string (:public keypair)))
-(def secret-key-string (bs/to-string (:secret keypair)))
+
+(defn save-keypair
+  "saves keypair to ~/.ssb-clj/secret"
+  [pair file-path]
+  (let [public-key-string (bs/to-string (:public keypair))
+        secret-key-string (bs/to-string (:secret keypair))]
+    (spit file-path (str "Public Key: " public-key-string))
+    (spit file-path (str "\nPrivate Key: " secret-key-string) :append true)))
