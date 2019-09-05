0c7be79a2c9d6ff29e3bdc337a930c75a5cdcd0b
Thomas Lisankie <thomas@tomlisankie.com>
2019-09-05 13:40:20 -0400

    Started using byte-streams library to convert ByteBuffer to string instead of my own function

--- src/ssb_clj/core.clj
+++ src/ssb_clj/core.clj
@@ -1,13 +1,7 @@
 (ns ssb-clj.core
-  (:require [caesium.crypto.sign :as sign]))
-
-(defn byte-buffer-to-string
-  "Converts the contents of a java.nio.ByteBuffer to a string."
-  [buffer]
-  (let [bytes (.array buffer)
-        string-from-byte-array (String. bytes)]
-    string-from-byte-array))
+  (:require [caesium.crypto.sign :as sign]
+            [byte-streams :as bs]))
 
 (def keypair (sign/keypair!))
-(def public-key-string (byte-buffer-to-string (:public keypair)))
-(def secret-key-string (byte-buffer-to-string (:secret keypair)))
+(def public-key-string (bs/to-string (:public keypair)))
+(def secret-key-string (bs/to-string (:secret keypair)))
