b2ac149b4d075c241a95f481923e851787a30ed2
Thomas Lisankie <thomas@tomlisankie.com>
2019-09-05 19:23:46 -0400

    Now encodes public and private keys as base64 (what it was supposed to do in the first place)

--- src/ssb_clj/core.clj
+++ src/ssb_clj/core.clj
@@ -1,13 +1,25 @@
 (ns ssb-clj.core
   (:require [caesium.crypto.sign :as sign]
-            [byte-streams :as bs]))
-
-(def keypair (sign/keypair!))
+            [byte-streams :as bs]
+            [clojure.data.codec.base64 :as b64]))
 
 (defn save-keypair
   "saves keypair to ~/.ssb-clj/secret"
   [pair file-path]
-  (let [public-key-string (bs/to-string (:public keypair))
-        secret-key-string (bs/to-string (:secret keypair))]
+  (let [public-key-string (bs/to-string (b64/encode (byte-array (.array (:public pair)))))
+        secret-key-string (bs/to-string (b64/encode (byte-array (.array (:secret pair)))))]
     (spit file-path (str "Public Key: " public-key-string))
     (spit file-path (str "\nPrivate Key: " secret-key-string) :append true)))
+
+(defn show-user-id
+  "Given a public key as a string, show the public ID of the user according to the format defined in the Scuttlebutt Protocol"
+  [public-key-string]
+  (str "@" public-key-string ".ed25519"))
+
+(defn create-new-user
+  "Creates a new user profile"
+  [file-path]
+  (let [keypair (sign/keypair!)]
+    (save-keypair keypair file-path)
+    (show-user-id (bs/to-string (b64/encode (byte-array (.array (:public keypair))))))))
+  
