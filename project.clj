c829bed65ca4493a5de202d43c5be4f18862ca11
Thomas Lisankie <thomas@tomlisankie.com>
2019-09-06 14:05:41 -0400

    Added Aleph as a dependency for network programming

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [caesium "0.12.0"]
-                 [org.clojure/data.codec "0.1.1"]]
+                 [org.clojure/data.codec "0.1.1"]
+                 [aleph "0.4.6"]]
   :repl-options {:init-ns ssb-clj.core})
