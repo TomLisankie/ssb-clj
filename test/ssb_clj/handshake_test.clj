d20170460618a55581307361168b802bf2641537
Thomas Lisankie <id@tomlisankie.com>
2019-10-08 13:21:05 -0400

    Test for initial message with different network identifiers

--- test/ssb_clj/handshake_test.clj
+++ test/ssb_clj/handshake_test.clj
@@ -6,9 +6,9 @@
 
 (deftest test-initial-message
   "Tests to make sure initial message is sent and received correctly."
-  (let [server-long-term-key-pair (Signature$KeyPair/random)
+  (let [server-long-term-key-pair (random-key-pair)
         network-identifier (Bytes32/random)
-        client (create-handshake-client (Signature$KeyPair/random)
+        client (create-handshake-client (random-key-pair)
                                         network-identifier
                                         (.publicKey server-long-term-key-pair))
         server (create-handshake-server server-long-term-key-pair
@@ -18,3 +18,13 @@
         (read-hello client (create-hello server))]
     (is (= (shared-secret client) (shared-secret server)))
     (is (= (shared-secret-2 client) (shared-secret-2 server)))))
+
+(deftest test-initial-message-different-network-identifier
+  (let [server-long-term-key-pair (random-key-pair)
+        client (create-handshake-client (random-key-pair)
+                                        (Bytes32/random)
+                                        (.publicKey server-long-term-key-pair))
+        server (create-handshake-server server-long-term-key-pair
+                                        (Bytes32/random))
+        initial-message (create-hello client)]
+    (is (thrown? HandshakeException (read-hello server initial-message)))))
